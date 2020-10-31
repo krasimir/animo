f889341e46a6e17a9c098e6334eaa68d909a89d4
Krasimir Tsonev <krasimir@outset.ws>
2020-10-31 15:40:37 +0200

    Wrapping up

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## [0.2.0] - Renaming tween_manager to manager + updating readme
+## [0.2.0] - Renaming tween_manager to manager + updating readme + unit test
 
 ## [0.1.0] - Initial version
--- test/manager_test.dart
+++ test/manager_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tween_manager/manager.dart';
+import 'package:tween_manager/tween_manager.dart';
 
 class TestWidget extends StatelessWidget {
   Widget build(context) {
